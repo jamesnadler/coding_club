a031b815404e40f2da104c12f8159e55c086d10b
james nadler <jnadler2022@fau.edu>
2023-10-04 20:02:42 +0000

    modified:   day2.py

--- day2.py
+++ day2.py
@@ -25,3 +25,23 @@ elif x == 6: # elif is short for else if
     print("x is equal to 6")
 else:
     print ("x is neither 5 or 6")
+
+# While Loops
+while x == 5:
+    x = int(input("Enter a number: "))
+
+
+# Lists
+people = ["baron", "aji", "matthew"]
+for person in people:
+    print(person + " is fruity")
+
+while True:
+    direction = input("Where are you going? left, right, or forward: ")
+    print("You went " + direction)
+    if direction == "left":
+        print("Kill yourself")
+        break
+    if direction == "right":
+        print("Nice choice")
+        break
